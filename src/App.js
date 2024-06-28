6b571793f19d04430d4e7724a2d9a3998fe75ffd
TRIZ <luiz.rech@conffops.com.br>
2024-06-28 16:37:01 -0300

    changes 28/06 13

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ function App() {
   function handleChangeName(nome){
     setPessoa(nome)
   }
+  function returnName(nome){
+    setPessoa(nome)
+  }
   return (
-    <div className="App">
-      <h1>Meu primeiro App</h1>
+    <div class = 'container container-light bg-light d-flex justify-content-between' className="App">
+      <h1 class='h1'>Meu primeiro App</h1>
       <h2>{pessoa}</h2>
       <MostraNome pessoa="Allan" idade={178}/>
-      <button onClick={() =>handleChangeName("Thalia")}>
+      <button class='btn btn-primary' onClick={() =>handleChangeName("Thalia")}>
         Mudar nome
+      </button>
+      <button class='btn btn-primary' onClick={() => returnName("Professor de TI")}>
+        Voltar nome
       </button>
 
     </div>

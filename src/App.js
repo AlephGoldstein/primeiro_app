6ebec6bb0b50e9fc88a4e89df6d3ad36c3aa1a13
TRIZ <luiz.rech@conffops.com.br>
2024-06-28 15:44:49 -0300

    changes 28/06 10

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>Meu primeiro App</h1>
+      <MostraNome/>
     </div>
   );
 }
 
 export default App;
+
+function MostraNome(){
+  return (
+    <span>Ohayo Allan-chan</span>
+  );
+}

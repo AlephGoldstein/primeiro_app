cdf0a60179d2b58b761b77a506b0c97cf1056cf7
Aleph Goldstein <luiz.rech@conffops.com.br>
2024-07-03 14:44:29 -0300

    changes 03/07 5

--- src/App.js
+++ src/App.js
@@ -1,75 +1,107 @@
 import { useState } from "react";
-
+/*
+import MostraNome from "./components/MostraNome"
 function App() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [idade, setIdade] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Aqui você poderia enviar os dados para algum lugar, exibir, etc.
-    console.log(`Nome: ${nome}, Email: ${email}, Idade: ${idade}`);
-  };
-
+  const [pessoa, setPessoa] = useState("Professor de TI")
+  function handleChangeName(nome){
+    setPessoa(nome)
+  }
+  function returnName(nome){
+    setPessoa(nome)
+  }
   return (
-    <div className="container container-light bg-light d-flex justify-content-between">
-      <div className="col-md-6">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="nome" className="form-label">
-              Nome
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="nome"
-              aria-describedby="emailHelp"
-              value={nome}
-              onChange={(e) => setNome(e.target.value)}
-            />
-          </div>
+    <div className = 'container container-light bg-light d-flex justify-content-between' classNameName="App">
+      <h1 className='h1'>Meu primeiro App</h1>
+      <h2>{pessoa}</h2>
+      <MostraNome pessoa="Allan" idade={178}/>
+      <button className='btn btn-primary' onClick={() =>handleChangeName("Thalia")}>
+        Mudar nome
+      </button>
+      <button className='btn btn-primary' onClick={() => returnName("Professor de TI")}>
+        Voltar nome
+      </button>
 
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="idade" className="form-label">
-              Idade
-            </label>
-            <input
-              type="number"
-              className="form-control"
-              id="idade"
-              aria-describedby="emailHelp"
-              value={idade}
-              onChange={(e) => setIdade(e.target.value)}
-            />
-          </div>
-
-          <button type="submit" className="btn btn-primary">
-            Enviar
-          </button>
-        </form>
-
-        <div>
-          <h1>{nome}</h1>
-          <h1>{email}</h1>
-          <h1>{idade}</h1>
-        </div>
-      </div>
     </div>
   );
 }
 
 export default App;
+*/
+/*
+function MeuForms(){
+const[nome,setNome] = useState(" ");
+const handleChange = (event) => {
+  setNome(event.target.value)
+};
+const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(`Olá ${nome}`)
+}
+return(
+  <div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nome:
+        <input type="text" value={nome} onChange={handleChange} className="form-control" />
+      </label>
+      <input type="submit" value="Enviar" className="btn btn-primary" />
+    </form>
+  </div>
+)
+}
+export default MeuForms;
+*/
+function App(){
+  const [nome, setNome] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const [idade, setIdade] = useState(" ");
+
+const handleRegister = (e) =>{
+  e.preventDefault();
+  console.log({nome},{email},{idade})
+  };
+return(
+
+  <div className="container container-light bg-light d-flex justify-content-between">
+
+    <div className="col-md-6">
+
+      <form onSubmit={handleRegister}>
+
+        <div className="mb-3">
+
+          <label for="exampleInputEmail1" className="form-label">Nome</label>
+
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={nome} onChange={(e) => setNome(e.target.value)} />
+
+        </div>
+
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">Email</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        </div>
+
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">Idade</label>
+          <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={idade} onChange={(e) => setIdade(e.target.value)} />
+
+        </div>
+
+        <button type="submit" className="btn btn-primary">Enviar</button>
+        
+        <div>
+
+          <h1>{nome}</h1>
+
+          <h1>{email}</h1>
+
+          <h1>{idade}</h1>
+        </div>
+
+      </form>
+      </div>
+      </div>
+)
+}
+export default App
